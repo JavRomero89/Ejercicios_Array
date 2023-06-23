946e92df7c10216381b1ee64e92871c3b77da96a
JavRomero89 <javieromero23@gmail.com>
2023-06-23 18:24:37 +0200

    Advance

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("-----STRINGS-----");
+let Javier = "Soy un noob"
+console.log(Javier);
+
+Javier = "No entiendo nada"
+console.log(Javier);
+
+console.log(Javier[5]);
+
+Javier[0] = "A"
+console.log(Javier);
+
+console.log("-----ARRAYS-----");
+
+let classNames = ["Santiago", "Morales", "Otniel", "Oscar", "Javier"]
+console.log(classNames[4]);
+
+classNames[5] = "Javier"
+console.log(classNames);
+
+let personalInfo = ["Javier", "Romero Castro", "Callao", 34]
+alert(personalInfo[0],personalInfo[1], "nacido en", personalInfo[2], "ha conseguido convertirse en desarrolador web a sus", personalInfo[3], "años.");
+
